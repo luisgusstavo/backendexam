69e60ceb3c0ef6e38ce05a0eac46382bee98001e
Luis Gustavo <luisgusstavo@live.com>
2023-01-04 11:34:29 -0300

    Initial Commit. Spring Startr setup

---
+++ src/main/java/com/attornatus/backendexam/BackendexamApplication.java
@@ -0,0 +1,13 @@
+package com.attornatus.backendexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendexamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendexamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/attornatus/backendexam/BackendexamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.attornatus.backendexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendexamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
