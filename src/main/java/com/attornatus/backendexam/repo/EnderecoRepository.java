120e4f677b4e8b6705b23470058e83afd730217e
Luis Gustavo <luisgusstavo@live.com>
2023-01-05 00:17:24 -0300

    Initial Commit. 1) Spring Startr setup. 2) Entities, repositories and services defined.

--- src/main/java/com/attornatus/backendexam/repo/EnderecoRepository.java
+++ src/main/java/com/attornatus/backendexam/repo/EnderecoRepository.java
@@ -3,10 +3,18 @@ package com.attornatus.backendexam.repo;
 import com.attornatus.backendexam.domain.Endereco;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * Repositório de Endereços
+ * Algumas operações foram restritas por questões de segurança.
+ * Somente as operações de criação, consulta e listagem foram mantidas.
+ */
+@RepositoryRestResource(collectionResourceRel = "enderecos",path = "enderecos")
 public interface EnderecoRepository extends CrudRepository<Endereco,Integer> {
     /**
      * Lista os endereços associados a uma pessoa
@@ -22,4 +30,36 @@ public interface EnderecoRepository extends CrudRepository<Endereco,Integer> {
      */
     @Query("select e from Endereco e where e.pessoa.id = ?1 and e.principal = true")
     Optional<Endereco> findEnderecoPrincipal(Integer id);
+
+    @Override
+    @RestResource(exported = false)
+    <S extends Endereco> Iterable<S> saveAll(Iterable<S> entities);
+
+    @Override
+    @RestResource(exported = false)
+    boolean existsById(Integer integer);
+
+    @Override
+    @RestResource(exported = false)
+    Iterable<Endereco> findAllById(Iterable<Integer> integers);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteById(Integer integer);
+
+    @Override
+    @RestResource(exported = false)
+    void delete(Endereco entity);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAllById(Iterable<? extends Integer> integers);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll(Iterable<? extends Endereco> entities);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll();
 }
--- src/main/java/com/attornatus/backendexam/repo/PessoaRepository.java
+++ src/main/java/com/attornatus/backendexam/repo/PessoaRepository.java
@@ -2,7 +2,44 @@ package com.attornatus.backendexam.repo;
 
 import com.attornatus.backendexam.domain.Pessoa;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
 
+/**
+ * Repositório de Pessoas
+ * Algumas operações foram restritas por questões de segurança.
+ * Somente as operações de criação, listagem e edição foram mantidas.
+ */
 public interface PessoaRepository extends CrudRepository<Pessoa,Integer> {
+    @Override
+    @RestResource(exported = false)
+    <S extends Pessoa> Iterable<S> saveAll(Iterable<S> entities);
+
+    @Override
+    @RestResource(exported = false)
+    boolean existsById(Integer integer);
+
+    @Override
+    @RestResource(exported = false)
+    Iterable<Pessoa> findAllById(Iterable<Integer> integers);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteById(Integer integer);
+
+    @Override
+    @RestResource(exported = false)
+    void delete(Pessoa entity);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAllById(Iterable<? extends Integer> integers);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll(Iterable<? extends Pessoa> entities);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll();
 
 }
